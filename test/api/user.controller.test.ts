4cb189ce6a5de5c0f3bbddda78618005709ea261
Varazdat Stepanyan <developervaryan@gmail.com>
2020-11-29 22:18:04 +0400

    Fix test

--- test/api/user.controller.test.ts
+++ test/api/user.controller.test.ts
@@ -1,12 +1,19 @@
 import 'mocha';
 import { expect } from 'chai';
-import { API_URL, loginWithDefaultUser, req } from '../common';
+import {
+  API_URL,
+  loginWithDefaultUser,
+  makeSureAllIsReady,
+  req,
+} from '../common';
 
 describe('User endpoints', () => {
   let token;
+  let userId;
 
   before(async () => {
     //get token
+    await makeSureAllIsReady();
     const resToken = await loginWithDefaultUser();
     token = resToken.body.token;
   });
@@ -18,12 +25,15 @@ describe('User endpoints', () => {
       .expect(200)
       .then((response) => {
         expect(response.body).to.be.an('array');
+        userId =
+          response.body[Math.floor(Math.random() * (response.body.length - 1))]
+            .id;
       });
   });
 
   it('should retrieve user by id', () => {
     return req
-      .get(`${API_URL}/users/1`)
+      .get(`${API_URL}/users/${userId}`)
       .set('Authorization', token)
       .then((response) => {
         expect(response.body).to.be.an('object');
--- test/common.ts
+++ test/common.ts
@@ -1,29 +1,44 @@
 import Server from '../app';
 import request from 'supertest';
-import AccessEntityInterface from '../modules/acl/entities/access.entity.interface';
+import '../app/config/database';
+import event from '../app/common/event';
+import { EVENTS } from '../app/config/events';
+import UserSchema from '../modules/user/entities/user.schema';
+import AccessSchema from '../modules/acl/entities/access.schema';
 
 export const API_URL = '/api/v1';
 
 export const req = request(Server);
 
-export const getDefaultUser = () => {
+export const makeSureAllIsReady = async (): Promise<any> => {
+  return new Promise((resolve) => {
+    event.on(EVENTS.DB_CONNECTED, resolve);
+  });
+};
+
+export const getDefaultUser = (): Omit<
+  UserSchema,
+  'email' | 'id' | 'firstName' | 'lastName'
+> => {
   return {
     username: 'varyan',
     password: 'secret',
+    permissions: [],
+    roles: [],
   };
 };
 
-export const loginWithDefaultUser = async () => {
+export const loginWithDefaultUser = async (): Promise<any> => {
   const user = await getDefaultUser();
   return req.post(`${API_URL}/auth`).send(user).expect(200);
 };
 
-export const TestPermission: AccessEntityInterface = {
+export const TestPermission: AccessSchema = {
   id: '1',
   name: '*',
 };
 
-export const TestRole: AccessEntityInterface = {
+export const TestRole: AccessSchema = {
   id: '2',
   name: 'moderator',
 };
