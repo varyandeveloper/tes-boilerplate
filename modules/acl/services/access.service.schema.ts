ba2b4349e2bacd55fe25f56e63c0e95c3e088c27
Varazdat Stepanyan <developervaryan@gmail.com>
2020-11-29 22:37:30 +0400

    Fixed filter path

--- modules/acl/services/access.service.schema.ts
+++ modules/acl/services/access.service.schema.ts
@@ -1,7 +1,7 @@
 import { DeepPartial } from 'typeorm';
 import RoleEntity from '../entities/role.entity';
 import AccessSchema from '../entities/access.schema';
-import AccessQueryFiler from '../filters/access.query.filer';
+import AccessQueryFiler from '../filters/access.query.filter';
 
 export default interface AccessServiceInterface {
   fetch(filter: AccessQueryFiler): Promise<AccessSchema>;
